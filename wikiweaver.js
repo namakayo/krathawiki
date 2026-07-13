d4beff5adb8937348461cefd095b002e1e9de90d
namakayo <purnamaaleron7@gmail.com>
2026-07-13 18:14:39 +0700

    update wikiweaver.js

--- wikiweaver.js
+++ wikiweaver.js
@@ -129,7 +129,9 @@ function parseStyle(all){
   pages=all;
 }
 function openPage(p){
+  console.log("tried to open: "+p)
   if(!(p in pages))return;
+  console.log("opened page: "+p)
   clearPage();
   page=p;
   currentPage=p;
@@ -328,6 +330,7 @@ function update(){
     if(window.location.search){
       const params=new URLSearchParams(window.location.search);
       let x=params.get("at");
+      console.log(x);
       if(x&&pages[x]!=null){
         openPage(x);
       }else{
